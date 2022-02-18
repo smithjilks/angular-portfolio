843e4d347c232f09d325ad60184b62e337e2bfd9
smithjilks <smithjilks@gmail.com>
2022-02-18 12:10:47 +0300

    Update projects urls

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { LandingComponent } from './landing/landing.component';
 
 const routes: Routes = [
   { path:'', component:LandingComponent },
+
 ];
 
 @NgModule({
--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -12,16 +12,18 @@ export class LandingComponent implements OnInit {
 
   constructor(private renderer: Renderer2) {}
 
-   downloadFile() {
-      const link = document.createElement('a');
-      link.setAttribute('target', '_blank');
-      link.setAttribute('href', 'https://drive.google.com/u/0/uc?id=1VTiGzphtFJ89tZ1W0QxJIYVv-kO10y8q&export=download');
-      link.setAttribute('download', `Jilks_Smith.pdf`);
-      link.click();
-      link.remove();
-   }
+  downloadFile() {
+    const link = document.createElement('a');
+    link.setAttribute('target', '_blank');
+    link.setAttribute('href', 'https://drive.google.com/u/0/uc?id=1VTiGzphtFJ89tZ1W0QxJIYVv-kO10y8q&export=download');
+    link.setAttribute('download', `Jilks_Smith.pdf`);
+    link.click();
+    link.remove();
+  }
 
-
+  showDialog() {
+    alert("Under construction. Check back soon!");
+  }
   ngOnInit(): void {
 
   }
